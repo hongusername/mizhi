8e3bbe864e73dfa4c413eed0c900920cb5a06986
Fairy0427 <maxuhui666@gmail.com>
2018-11-30 16:36:25 +0800

    日常提交，11。30

--- mizhi/src/main/java/cn/jishu/mizhi/hui/pay/config/AlipayConfig.java
+++ mizhi/src/main/java/cn/jishu/mizhi/hui/pay/config/AlipayConfig.java
@@ -18,10 +18,10 @@ public class AlipayConfig {
     public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8wRgququZy2Ogxu1mnAXBPRnSmDnGLjvw+7EE4pkU16vCOOEIicgCPrl3YcUfwg0GKvk2pzcow2yWckIAUwa5e/vihJvxTjrpx5kxszuBMfcdJd0lNhPAuLOr2eagGtTBrvokIrwEQvK9PLpahBV195OHEfRTpLkJ+hjvdIyHgK7yVnGzXILTPqVkA1LmzKUmTsLZ3MfMdNn2oQFHZRuCTBXKvelmWnewFQ43EFPaZmvMdLpnFY3NVbWPrji3qgGgMzDG3f9wp60JWwRKKrJyYpHxdYrdev6+q4M5JgoFg/44x8N5Q6EkNYJ1rOzlNTK1280ZuUW9HYa54KtAk5dQwIDAQAB";
 
     // 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-    public static String notify_url = "http://localhost:8080/notifyUrl";
+    public static String notify_url = "http://localhost/notifyUrl";
 
     // 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-    public static String return_url = "http://localhost:8080/returnUrl";
+    public static String return_url = "http://localhost/returnUrl";
 
     // 签名方式
     public static String sign_type = "RSA2";
